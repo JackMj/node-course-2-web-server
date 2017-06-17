1c5aeb83448dbc1d5e0c9f3d03fdc31e008c8885
JackMj <muzi.jack@gmail.com>
2017-06-17 21:47:21 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,66 @@
+var express = require('express');
+const hbs = require('hbs');
+
+
+var app = express();
+const fs = require('fs');
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase()
+})
+
+
+app.use((req,res,next)=>{
+     res.render('maintanance.hbs')
+})
+
+app.use((req,res,next)=>{
+    var now = new Date().toDateString();
+    var log = `${now} ${req.method} ${req.url}`;
+    
+     console.log(log);
+    fs.appendFile('server.log',log + '\n',(err)=>{
+        if(err)
+        {
+            console.log('an error occured!  ')
+        }
+    });
+
+    next(); 
+})
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        welcomeMessage:'Welcome to my website'
+        
+    })
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+        welcomeMessage:'About '
+       
+    })
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage:'unable to handle request'
+    });
+})
+
+app.listen(3000,()=>{
+    console.log('server is up');
+});
+
